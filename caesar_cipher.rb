672738772ab1cd81476f445993859cd363d19c1b
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-03-04 18:32:53 -0500

    Add solution for Caesar Cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,38 @@
+def caesar_cipher(str, shift_num)
+  alphabet_lower = ("a".."z").to_a
+  alphabet_upper = ("A".."Z").to_a
+  new_str = ''
+  str_arr = str.split('')
+  str_arr.each_with_index do |character, index|
+    if character == character.downcase && alphabet_lower.include?(character)
+      alphabet_lower.each_with_index do |letter, index|
+        if letter == character
+          if index + shift_num <= 25
+            new_str << alphabet_lower[index + shift_num]
+            break;
+          else
+            new_str << alphabet_upper[(index + shift_num) - 25 -1]
+            break;
+          end
+        end
+      end
+    elsif character == character.upcase && alphabet_upper.include?(character)
+      alphabet_upper.each_with_index do |letter, index|
+        if letter == character
+          if index + shift_num <= 25
+            new_str << alphabet_upper[index + shift_num]
+            break;
+          else
+            new_str << alphabet_upper[(index + shift_num) - 25 -1]
+            break;
+          end
+        end
+      end
+    else
+      new_str << character
+    end
+  end
+  puts new_str
+end
+
+caesar_cipher("What a string!", 5)
